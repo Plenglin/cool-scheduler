0ca041870de8f078168ed0c18b0da7d8b0dc9386
Astrid Augusta Yu <astridyu3.14@gmail.com>
2018-09-20 18:16:55 -0700

    Does it work? Let's wait and see....

--- src/main/java/com/ironpanthers/scheduler/command/Command.java
+++ src/main/java/com/ironpanthers/scheduler/command/Command.java
@@ -1,11 +1,43 @@
 package com.ironpanthers.scheduler.command;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public abstract class Command {
 
     private final Object lock = new Object();
-    private double lastCalled = 0.0;
-    private boolean initialized = false;
-    protected Scheduler scheduler;
+    private long lastCalled = 0L;
+    private Set<Subsystem> requiredSubsystems = new HashSet<Subsystem>();
+    private boolean isInitialized = false;
+    private boolean isTerminated = false;
+    Command next = null;
+    Command prev = null;
+
+    public boolean isInitialized() {
+        return isInitialized;
+    }
+
+    public boolean isTerminated() {
+        return isTerminated;
+    }
+
+    public Set<Subsystem> getRequiredSubsystems() {
+        return requiredSubsystems;
+    }
+
+    private void remove() {
+        prev.next = next;
+        next.prev = prev;
+    }
+
+    void append(Command command) {
+        command.prev = this;
+        next = command;
+    }
+
+    protected void require(Subsystem subsystem) {
+        requiredSubsystems.add(subsystem);
+    }
 
     /**
      * Blocks the current thread until this command is terminated.
@@ -20,41 +52,39 @@ public abstract class Command {
     /**
      * Called once and only once shortly after this command is added to Scheduler.
      */
-    public abstract void onInitialize();
+    public void onInitialize() {}
 
     /**
      * Called as fast as possible.
      * @param dt time since this was last called
      * @return should this command be called again?
      */
-    public abstract boolean onLoop(double dt);
+    public boolean onLoop(double dt) { return false; }
 
     /**
      * Called once and only once after onLoop returns false.
      * @param interrupted was this command interrupted by another command?
      */
-    public abstract void onTerminate(boolean interrupted);
+    public void onTerminate(boolean interrupted) {}
 
-    void initialize(Scheduler scheduler) {
-        lastCalled = System.currentTimeMillis();
-        this.scheduler = scheduler;
-    }
-
-    boolean call(Scheduler scheduler) {
-        if (!initialized) {
-            initialize(scheduler);
-            initialized = true;
-        }
-        boolean shouldCallAgain = onLoop(System.currentTimeMillis() - lastCalled);
-        lastCalled = System.currentTimeMillis();
-        return shouldCallAgain;
-    }
-
-    void terminateNormally() {
-        onTerminate(false);
-        synchronized (lock) {
-            lock.notifyAll();
+    final void _initialize() {
+        onInitialize();
+        isInitialized = true;
+        for (Subsystem subsystem: requiredSubsystems) {
+            subsystem.getCurrentCommand()._terminate(true);
         }
     }
 
+    final boolean _loop() {
+        long currentTime = System.currentTimeMillis();
+        boolean result = onLoop((currentTime - lastCalled) / 1000.0);
+        lastCalled = currentTime;
+        return result;
+    }
+
+    final void _terminate(boolean interrupted) {
+        onTerminate(interrupted);
+        isTerminated = true;
+        remove();
+    }
 }
--- src/main/java/com/ironpanthers/scheduler/command/Scheduler.java
+++ src/main/java/com/ironpanthers/scheduler/command/Scheduler.java
@@ -1,26 +1,88 @@
 package com.ironpanthers.scheduler.command;
 
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 import java.util.concurrent.LinkedBlockingQueue;
 
-public class Scheduler {
+public class Scheduler implements Iterable<Command> {
 
-    private LinkedBlockingQueue<Command> commands = new LinkedBlockingQueue<>();
-
+    private LinkedBlockingQueue<Command> commandsToAdd = new LinkedBlockingQueue<>();
+    private SentinelNode sentinel = new SentinelNode();
+    private Set<Subsystem> subsystems = new HashSet<>();
 
     public void registerSubsystem(Subsystem subsystem) {
+        subsystems.add(subsystem);
+    }
 
+    private void addNewCommands() {
+        Command command = sentinel.prev;
+        while (!commandsToAdd.isEmpty()) {
+            Command newCommand = commandsToAdd.poll();
+            newCommand._initialize();
+            command.append(newCommand);
+            command = newCommand;
+        }
+    }
+
+    private void executeCurrentCommands() {
+        Command command = sentinel.next;
+        while (command != sentinel) {
+            if (!command.isInitialized()) {
+                command._initialize();
+            }
+            if (!command._loop()) {
+                command._terminate(false);
+            }
+        }
     }
 
     /**
-     * Run this scheduler for 1 iteration.
-     * @throws InterruptedException
+     * Run 1 iteration of this scheduler.
      */
-    public void run() throws InterruptedException {
-        Command command = commands.take();
-        if (command.call(this)) {
-            commands.put(command);
-        } else {
-            //command.terminate();
+    public void run() {
+        addNewCommands();
+        executeCurrentCommands();
+    }
+
+    @Override
+    public Iterator<Command> iterator() {
+        return new Iterator<Command>() {
+            Command currentCommand = sentinel.next;
+            @Override
+            public boolean hasNext() {
+                return currentCommand == sentinel;
+            }
+
+            @Override
+            public Command next() {
+                Command out = currentCommand;
+                currentCommand = currentCommand.next;
+                return out;
+            }
+        };
+    }
+
+    private class SentinelNode extends Command {
+        SentinelNode() {
+            super();
+            next = this;
+            prev = this;
+        }
+
+        @Override
+        public void onInitialize() {
+            throw new IllegalStateException("SentinelNode should not be called!");
+        }
+
+        @Override
+        public boolean onLoop(double dt) {
+            throw new IllegalStateException("SentinelNode should not be called!");
+        }
+
+        @Override
+        public void onTerminate(boolean interrupted) {
+            throw new IllegalStateException("SentinelNode should not be called!");
         }
     }
 
